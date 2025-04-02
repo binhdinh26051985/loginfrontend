e2156a19b8bf163eebf3599a033300df675a1d3a
binhdinh26051985 <112796138+binhdinh26051985@users.noreply.github.com>
2025-04-02 15:54:27 +0700

    Update vite.config.js

--- order-app/vite.config.js
+++ order-app/vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
-// vite.config.js
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/loginfrontend/', // Must match your Vercel project name
-  build: {
-    emptyOutDir: true
-  }
-});
+  plugins: [react()],
+})
