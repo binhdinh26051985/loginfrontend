5d4aa3dc1c6568cae73d5510ce368ee1b6724fe8
binhdinh26051985 <112796138+binhdinh26051985@users.noreply.github.com>
2025-04-02 15:22:50 +0700

    Update vite.config.js

--- order-app/vite.config.js
+++ order-app/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  base: "/deploy-github/",
-  plugins: [react()],
+  base: '/loginfrontend/', // Must match your GitHub repo name exactly
+  plugins: [react()]
 })
